01913d20e7f9a5d868843eebe3c6ef4bdd1e91f5
Vipul Singhal <vipulsinghal02@gmail.com>
2023-09-22 14:14:36 -0400

    Modified the Add_test.R file to:
    1. Set credentials
    2. Set line for generating GitHub repo

--- Add_test.R
+++ Add_test.R
@@ -5,3 +5,10 @@ library(tidyverse)
 # install.packages("data.table")
 
 library(data.table)
+
+usethis:::create_github_token()
+
+# install.packages("gitcreds")
+gitcreds::gitcreds_set()
+
+usethis::use_github()
